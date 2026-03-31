5831e5065eaed341aa264cffcb86f557ded070b2
Justin-Blu3berry <jcwildman.jw@gmail.com>
2026-03-31 14:13:25 -0400

    Create viterbi.py

---
+++ project08/viterbi.py
@@ -0,0 +1,11 @@
+# Justin, Linh, Hongyuan
+# Project 8: Viterbi Algorithm
+
+# import statements
+
+def Viterbi():
+  pass
+
+if __name__ == "__main__":
+  # TODO: do something with a toy example for demo purposes here
+  Viterbi()
